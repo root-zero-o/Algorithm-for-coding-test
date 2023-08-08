66c0841db13f9bb127fbd352f5b29d810d0c9b12
Kim GeunYeong <97326130+root-zero-o@users.noreply.github.com>
2023-08-08 21:26:51 +0900

    Create 2588.곱셈.js

---
+++ baekjoon/2588.곱셈.js
@@ -0,0 +1,12 @@
+// 2023-08-08
+// https://www.acmicpc.net/problem/2588
+
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const A = Number(input[0]);
+const B = input[1];
+
+console.log(A * Number(B[2]));
+console.log(A * Number(B[1]));
+console.log(A * Number(B[0]));
+console.log(A * Number(B));
