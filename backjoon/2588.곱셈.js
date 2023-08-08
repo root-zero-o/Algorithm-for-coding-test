b4954abd332ec3014d66e6b3f77381f4a057956d
Kim GeunYeong <97326130+root-zero-o@users.noreply.github.com>
2023-08-08 21:22:24 +0900

    Create 2588.곱셈.js

---
+++ backjoon/2588.곱셈.js
@@ -0,0 +1,9 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const A = Number(input[0]);
+const B = input[1];
+
+console.log(A * Number(B[2]));
+console.log(A * Number(B[1]));
+console.log(A * Number(B[0]));
+console.log(A * Number(B));
