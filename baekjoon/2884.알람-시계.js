0cda1802ce606b139a535424ab51b3c6e9462e18
Kim GeunYeong <97326130+root-zero-o@users.noreply.github.com>
2023-08-08 21:53:53 +0900

    Create 2884.알람-시계.js

---
+++ baekjoon/2884.알람-시계.js
@@ -0,0 +1,16 @@
+// 2023-08-08
+// https://www.acmicpc.net/problem/2884
+
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let hour = Number(input[0].split(' ')[0]);
+let minute = Number(input[0].split(' ')[1]);
+
+if(minute < 45){
+    hour -= 1;
+    minute += 15;
+    if(hour < 0) hour = 23;
+} else minute -= 45;
+
+console.log(hour + " " + minute)
