5d62cad024235b34701905de09c8f37bca047fa5
Kim GeunYeong <97326130+root-zero-o@users.noreply.github.com>
2023-08-08 21:21:51 +0900

    Create 10869.사칙연산.js

---
+++ backjoon/10869.사칙연산.js
@@ -0,0 +1,12 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const data = input[0].split(' ');
+
+const A = parseInt(data[0]);
+const B = parseInt(data[1]);
+
+console.log(A+B);
+console.log(A-B);
+console.log(A*B);
+console.log(parseInt(A/B));
+console.log(A%B);
