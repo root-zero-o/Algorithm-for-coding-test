f6a2866cfa1fa7397628fe77601a077fb0c69d7c
Kim GeunYeong <97326130+root-zero-o@users.noreply.github.com>
2023-08-08 21:00:26 +0900

    Create 10998.AxB.js

---
+++ backjoon/10998.AxB.js
@@ -0,0 +1,8 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const line = input[0].split(' ');
+
+const A = parseInt(line[0]);
+const B = parseInt(line[1]);
+
+console.log(A * B);
