5b34d30659bb95b586eb0d790e15cbb8b04068e5
Kim GeunYeong <97326130+root-zero-o@users.noreply.github.com>
2023-08-08 21:29:15 +0900

    Create 1000.A+B.js

---
+++ baekjoon/1000.A+B.js
@@ -0,0 +1,11 @@
+// 2023-08-08
+// https://www.acmicpc.net/problem/1000
+
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');  // ['1 2']
+
+const line = input[0].split(' ');  // ['1', '2']
+const A = parseInt(line[0]); // 1
+const B = parseInt(line[1]);  // 2
+
+console.log(A+B);
