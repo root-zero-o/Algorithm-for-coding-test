49f971766725d775007ee0618ff64a00848ad3d0
root-zero-o <rootzero17@gmail.com>
2023-08-30 21:11:31 +0900

    우선순위 큐 코드 공부

--- library/priority-queue.js
+++ library/priority-queue.js
@@ -1,36 +1,25 @@
 // 2023-08-29
 //https://github.com/ndb796/priorityqueuejs/blob/master/index.js
 
-/**
- * Expose `PriorityQueue`.
- */
+
+// PriorityQueue를 모듈화한다.
+
 module.exports = PriorityQueue;
 
-/**
- * Initializes a new empty `PriorityQueue` with the given `comparator(a, b)`
- * function, uses `.DEFAULT_COMPARATOR()` when no function is provided.
- *
- * The comparator function must return a positive number when `a > b`, 0 when
- * `a == b` and a negative number when `a < b`.
- *
- * @param {Function}
- * @return {PriorityQueue}
- * @api public
- */
+
+
+/** INIT */
+// comparator(a, b)가 주어지면 사용하고 없으면 DEFAULT_COMPARATOR 사용
+// 힙은 complete binary tree이기 때문에 배열로 표현(메모리 손실이 없다)
+// comparator : a가 크면 양수, b가 크면 음수를 return 해야 함
+
 function PriorityQueue(comparator) {
   this._comparator = comparator || PriorityQueue.DEFAULT_COMPARATOR;
-  this._elements = [];
+  this._elements = []; 
 }
 
-/**
- * Compares `a` and `b`, when `a > b` it returns a positive number, when
- * it returns 0 and when `a < b` it returns a negative number.
- *
- * @param {String|Number} a
- * @param {String|Number} b
- * @return {Number}
- * @api public
- */
+/** DEFAULT COMPARATOR */
+
 PriorityQueue.DEFAULT_COMPARATOR = function(a, b) {
   if (typeof a === 'number' && typeof b === 'number') {
     return a - b;
@@ -44,48 +33,60 @@ PriorityQueue.DEFAULT_COMPARATOR = function(a, b) {
   }
 };
 
-/**
- * Returns whether the priority queue is empty or not.
- *
- * @return {Boolean}
- * @api public
- */
+/** SIZE */
+// 배열의 length를 return
+PriorityQueue.prototype.size = function() {
+  return this._elements.length;
+};
+
+/** IS EMPTY */
+// 배열이 비어있는지 확인
 PriorityQueue.prototype.isEmpty = function() {
   return this.size() === 0;
 };
 
-/**
- * Peeks at the top element of the priority queue.
- *
- * @return {Object}
- * @throws {Error} when the queue is empty.
- * @api public
- */
+
+/** PEEK */
+// 가장 큰 값을 return
+// max heap - 루트 노드가 가장 크기 때문에 index 0을 return
+// 비어있으면 error
 PriorityQueue.prototype.peek = function() {
   if (this.isEmpty()) throw new Error('PriorityQueue is empty');
 
   return this._elements[0];
 };
 
-/**
- * Dequeues the top element of the priority queue.
- *
- * @return {Object}
- * @throws {Error} when the queue is empty.
- * @api public
- */
+
+/** COMPARE */
+// comparator 함수를 이용해 대소 비교
+// a가 크면 양수, b가 크면 음수 return
+PriorityQueue.prototype._compare = function(a, b) {
+  return this._comparator(this._elements[a], this._elements[b]);
+};
+
+
+/** SWAP */
+// a, b의 자리를 바꿈
+PriorityQueue.prototype._swap = function(a, b) {
+  var aux = this._elements[a];
+  this._elements[a] = this._elements[b];
+  this._elements[b] = aux;
+};
+
+/** DEQUEUE */
+// 원소 삭제 - 가장 큰 값(루트 노드)을 삭제함
 PriorityQueue.prototype.deq = function() {
-  var first = this.peek();
-  var last = this._elements.pop();
+  var first = this.peek(); // 배열의 첫 번째 원소(루트 노드)
+  var last = this._elements.pop(); // 배열의 마지막 원소(pop으로 삭제 - length가 줄어야 하므로)
   var size = this.size();
 
   if (size === 0) return first;
 
-  this._elements[0] = last;
-  var current = 0;
+  this._elements[0] = last; // 루트 노드를 삭제하고 마지막 last 원소를 루트 노드 자리로 이동
+  var current = 0; // 현재 이동한 노드의 index는 0
 
   while (current < size) {
-    var largest = current;
+    var largest = current; 
     var left = (2 * current) + 1;
     var right = (2 * current) + 2;
 
@@ -106,19 +107,14 @@ PriorityQueue.prototype.deq = function() {
   return first;
 };
 
-/**
- * Enqueues the `element` at the priority queue and returns its new size.
- *
- * @param {Object} element
- * @return {Number}
- * @api public
- */
+/** ENQUEUE */
+// 원소를 더함
 PriorityQueue.prototype.enq = function(element) {
-  var size = this._elements.push(element);
-  var current = size - 1;
+  var size = this._elements.push(element); // 마지막에 추가
+  var current = size - 1; // 현재 추가된 노드의 index
 
   while (current > 0) {
-    var parent = Math.floor((current - 1) / 2);
+    var parent = Math.floor((current - 1) / 2); // 부모 노드의 index
 
     if (this._compare(current, parent) <= 0) break;
 
@@ -129,47 +125,10 @@ PriorityQueue.prototype.enq = function(element) {
   return size;
 };
 
-/**
- * Returns the size of the priority queue.
- *
- * @return {Number}
- * @api public
- */
-PriorityQueue.prototype.size = function() {
-  return this._elements.length;
-};
 
-/**
- *  Iterates over queue elements
- *
- *  @param {Function} fn
- */
+/** ITERATE */
+// 반복
 PriorityQueue.prototype.forEach = function(fn) {
   return this._elements.forEach(fn);
 };
 
-/**
- * Compares the values at position `a` and `b` in the priority queue using its
- * comparator function.
- *
- * @param {Number} a
- * @param {Number} b
- * @return {Number}
- * @api private
- */
-PriorityQueue.prototype._compare = function(a, b) {
-  return this._comparator(this._elements[a], this._elements[b]);
-};
-
-/**
- * Swaps the values at position `a` and `b` in the priority queue.
- *
- * @param {Number} a
- * @param {Number} b
- * @api private
- */
-PriorityQueue.prototype._swap = function(a, b) {
-  var aux = this._elements[a];
-  this._elements[a] = this._elements[b];
-  this._elements[b] = aux;
-};
